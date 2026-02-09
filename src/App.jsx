de9eba927ef633411cf9996c3d08c3e8988362b3
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-09 15:25:42 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import Title from "./components/Title";
-import Counter from "./components/Counter";
 import { Outlet } from "react-router-dom";
 
 import NewYear from "./assets/fundo.jpg";
 
-import useCountdown from "./hooks/useCountdown";
-
 import "./App.css";
 
 function App() {
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2027 00:00:00");
-
   return (
     <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
       <div className="container">
-        <Title title="Contagem regressiva para 2027" />
-        <div className="countdown-container">
-          <Counter title="Dias" number={day} />
-          <Counter title="Horas" number={hour} />
-          <Counter title="Minutos" number={minute} />
-          <Counter title="Segundos" number={second} />
-        </div>
+        <Outlet />
       </div>
     </div>
   );
---
+++ src/routes/Countdown.jsx
@@ -0,0 +1,21 @@
+import Title from "../components/Title";
+import Counter from "../components/Counter";
+import useCountdown from "../hooks/useCountdown";
+
+const Countdown = () => {
+  const [day, hour, minute, second] = useCountdown("Jan 1, 2027 00:00:00");
+
+  return (
+    <>
+      <Title title="Contagem regressiva para 2027" />
+      <div className="countdown-container">
+        <Counter title="Dias" number={day} />
+        <Counter title="Horas" number={hour} />
+        <Counter title="Minutos" number={minute} />
+        <Counter title="Segundos" number={second} />
+      </div>
+    </>
+  );
+};
+
+export default Countdown;
