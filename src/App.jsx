4153914aa39c0c29c1c8c55a3652ff1140d76ebc
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-09 14:04:18 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Title from "./components/Title";
+import Counter from "./components/Counter";
+
+import NewYear from "./assets/fundo.jpg";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
       <div className="container">
         <Title title="Contagem regressiva para 2027" />
         <div className="countdown-container">
-          <p>2 dias</p>
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
         </div>
       </div>
     </div>
