00c2cd8410c1cba882dbd04031e9ba31e8a1279d
Rafael Toledo Souza <ch1at@outlook.com>
2026-02-10 09:32:16 -0300

    mudar cor

--- src/routes/Countdown.jsx
+++ src/routes/Countdown.jsx
@@ -12,16 +12,20 @@ const Countdown = () => {
 
   if (!event) return <Navigate to="/" />;
 
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2027 00:00:00");
+  const eventTitle = event.title;
+
+  const eventColor = event.color;
+
+  const [day, hour, minute, second] = useCountdown(event.date);
 
   return (
     <>
-      <Title title="Contagem regressiva para 2027" />
+      <Title title={eventTitle} eventColor={eventColor} />
       <div className="countdown-container">
-        <Counter title="Dias" number={day} />
-        <Counter title="Horas" number={hour} />
-        <Counter title="Minutos" number={minute} />
-        <Counter title="Segundos" number={second} />
+        <Counter title="Dias" number={day} eventColor={eventColor} />
+        <Counter title="Horas" number={hour} eventColor={eventColor} />
+        <Counter title="Minutos" number={minute} eventColor={eventColor} />
+        <Counter title="Segundos" number={second} eventColor={eventColor} />
       </div>
     </>
   );
